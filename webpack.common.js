a86da025217dc5b2d74ea162e6dc1c3efca4e13a
deaxmachina <d.y.bankova@gmail.com>
2021-08-09 19:38:30 +0900

    most of the code

---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js"
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ]
+  }
+}
